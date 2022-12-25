17616a07741628d0cf7b7ed43f794b690c8db75d
Marco Visaya <marco.visaya527@gmail.com>
2022-12-25 22:45:40 +0800

    Update layout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 // Add code to import the components
-import { AppProvider } from './context/AppContext';
+import { AppProvider } from "./context/AppContext";
 import Budget from "./components/Budget";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseTotal from "./components/ExpenseTotal";
 import Remaining from "./components/Remaining";
 import AllocationForm from "./components/AllocationForm";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import DropdownCurrency from "./components/DropdownCurrency";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const App = () => {
-    return (
-        <AppProvider>
-            <div className='container'>
-                <h1>Company's Budget Allocation</h1>
-                <Row>
-                <Col><Budget/></Col>
-                <Col><Remaining/></Col>
-                <Col><ExpenseTotal/></Col>                
-                </Row>
-                <ExpenseList/>
-                <h2>Change allocation</h2>
-                <AllocationForm/>
-            </div>
-        </AppProvider>
-    );
+  return (
+    <AppProvider>
+      <div className="container">
+        <h1>Company's Budget Allocation</h1>
+        <Row>
+          <Col>
+            <Budget />
+          </Col>
+          <Col>
+            <Remaining />
+          </Col>
+          <Col>
+            <ExpenseTotal />
+          </Col>
+          <Col>
+            <DropdownCurrency />
+          </Col>
+        </Row>
+        <h2>Allocation</h2>
+
+        <ExpenseList />
+        <h2>Change allocation</h2>
+        <AllocationForm />
+      </div>
+    </AppProvider>
+  );
 };
 export default App;
