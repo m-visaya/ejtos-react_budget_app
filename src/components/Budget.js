55bd1255aee24cc8eede20cd1761eb9f72d5185c
Marco Visaya <marco.visaya527@gmail.com>
2022-12-25 22:48:44 +0800

    Set budget field as editable

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,15 +1,42 @@
-
-import React, { useContext } from 'react';
-import { AppContext } from '../context/AppContext';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+  const { budget, dispatch, currency } = useContext(AppContext);
+  const [currbudget, setBudget] = useState(budget);
+  const { expenses } = useContext(AppContext);
 
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
+  const updateBudget = (event) => {
+    const value = event.target.value;
+    if (value < totalExpenses) {
+      alert("You cannot reduce the budget lower than the spending");
+      return;
+    }
+    setBudget(value);
+    dispatch({
+      type: "SET_BUDGET",
+      payload: value,
+    });
+  };
+
+  return (
+    <div className="alert alert-secondary">
+      <span>
+        Budget: {currency}
+        <input
+          type="number"
+          id="budget"
+          value={budget}
+          style={{ size: 10 }}
+          onChange={updateBudget}
+        ></input>
+      </span>
+    </div>
+  );
 };
 
 export default Budget;
